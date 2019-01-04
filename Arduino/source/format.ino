a4a4a7737ff620cc7b5c6cca42c416f4b127d5d5
Joe S <kenwood364@gmail.com>
2019-01-04 18:42:51 -0500

    init to comit it

---
+++ Arduino/source/format.ino
@@ -0,0 +1,9 @@
+//char angle_v[4];
+//char distance_v[4];
+
+void format(float angle, float distance)
+{
+  Serial.print(angle);
+  Serial.print("/");
+  Serial.println(distance);
+}
---
+++ Arduino/source/source.ino
@@ -0,0 +1,22 @@
+float angle;
+float distance;
+
+void setup() 
+{
+  Serial.begin(57600);
+  randomSeed(analogRead(0));
+}
+
+void loop()
+{
+  while(angle <= 360)
+  {
+    angle++; //Replace me!
+    distance = random(80,100); //Replace me!
+
+    format(angle,distance);
+    delay(1);
+  }
+
+  angle = 0;
+}
